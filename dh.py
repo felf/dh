326c413c3a014f11492a2dbadb57b39f6d1928e4
Frank Steinmetzger <dev@felsenfleischer.de>
2015-02-08 13:59:00 +0100

    chg: shorter help text to fit on one line

--- dh.py
+++ dh.py
@@ -238,7 +238,7 @@ def parse_arguments():  # {{{1
 
     parser.add_argument(
         'locations', metavar='dir', type=str, nargs='*',
-        help='a list of directories to parse (default is current dir)')
+        help='a list of directories to parse (default is .)')
     group = parser.add_mutually_exclusive_group()
     group.add_argument(
         '-q', '--quiet', action='count', default=0,
