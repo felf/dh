ad244131722efe75b3e0d297755dff41e84f79cc
Frank Steinmetzger <dev@felsenfleischer.de>
2015-02-11 19:03:51 +0100

    fix: missing dot in relative path output

--- dh.py
+++ dh.py
@@ -541,7 +541,7 @@ def process_files(filenum_width, path, files, checksum_files):  # {{{1
     if not args.create and not args.update and not checksum_files:
         if args.quiet < 3 and not args.no_missing_checksums:
             WARN("'{0}'".format(
-                path[len(cwd):] if path.startswith(cwd) else path),
+                "." + path[len(cwd):] if path.startswith(cwd) else path),
                  msg="No checksum file: ")
         State.md5_missing += 1
         return 0
